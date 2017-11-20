f2425212202373080f513e0c3c52fa97e33285e9
monolifeYw <lyw31136@naver.com>
2017-11-20 20:33:14 +0900

    [Server] set to resource path for router path

--- server-conf/server-webpack-router.js
+++ server-conf/server-webpack-router.js
@@ -10,6 +10,27 @@ function extendLocals(resLocals, opts = {}) {
   return Object.assign(resLocals, Locals, opts);
 }
 
+// render page
+function render(page) {
+  return function render(req, res) {
+    res.locals = extendLocals(res.locals, {
+      renderPage: page,
+    });
+    /**
+     * Router 내 render 설정
+     * @example
+     * - res.render('layouts/pc/test', { title: 'Ryan', layout: 'pc/default' });
+     * -> pc/default 내에 있는 {{{body}}} 내에 {app.set('views', PATHS.VIEW_DIR);}/layouts/pc/test.hbs 의 내용 삽입
+     */
+    res.render(page, { layout: 'pc/default' });
+
+    /**
+     * @todo express Can't set headers after they are sent
+     */
+    // next();
+  };
+}
+
 router
   .route('/')
   .get((req, res) => {
@@ -22,6 +43,11 @@ router
      */
     res.render('pages/pc/main', { layout: 'pc/default' });
   });
+
+router
+  .route('/main')
+  .get(render('pages/pc/main'));
+
 
 // error (404)
 router.use(function (req, res, next) {
