78ff5c04ad8b574f496df822e365fefd0b5884e8
monolifeYw <lyw31136@naver.com>
2017-12-05 00:29:32 +0900

    [Server] add-module-path 주석 추가

--- server.js
+++ server.js
@@ -11,6 +11,9 @@
  * module path 지정
  * Node.js 모듈 검색 경로에 최상위 수준 앱 모듈 전용 디렉토리를 추가
  * @see https://www.npmjs.com/package/app-module-path
+ *
+ * node 에서 실행시에만 적용이 되므로,
+ * resource(src/js/~~~) 에서 require 설정시 webpack 에서 compile시를 감안하여 상대경로로 지정한다.
  */
 require('app-module-path').addPath(process.cwd());
 
